1e49824ec645d7cf6e08ac8a1f77b10bd0f0e162
Andrei Breban <abreban@enermech.local>
2016-07-20 16:00:15 +0100

    services and dependency injection

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ProductList } from './products/product-list.component';
+import { ProductService} from './products/product.service';
 
 @Component({
     selector: 'pm-app',
     templateUrl: 'app/app.component.html',
-    directives: [ProductList]
+    directives: [ProductList],
+    providers: [ProductService]
 })
 export class AppComponent { 
     pageTitle:string = 'Acme Product Management';
